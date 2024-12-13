f89be75fda323d2bb91b9dea7892e1be1ec86f90
Denys <59747329+krdq-sourse@users.noreply.github.com>
2024-12-13 17:15:06 +0100

    Update master.js

--- master.js
+++ master.js
@@ -1,12 +1,26 @@
 import {Worker} from 'node:worker_threads';
 import * as os from 'node:os';
+import fs from 'node:fs';
+import path from 'path';
+
 
 const numWorkers = os.cpus().length/4;
 console.log('Number of threads:', numWorkers);
 
 const metrics = new Array(numWorkers).fill(0);
 const workers = [];
-const tail = 'ya';
+const tail = 'BRUH';
+
+function appendToFile(data) {
+  fs.appendFile(logFilePath, data + '\n', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    }
+  });
+}
+
+const logFilePath = path.join('./', 'mining_results.log');
+
 
 for (let i = 0; i < numWorkers; ++i) {
   const worker = new Worker('./worker.js', { workerData: tail });
@@ -25,6 +39,8 @@ for (let i = 0; i < numWorkers; ++i) {
     } else if (message.type === 'result') {
       console.log(`Worker ${i} mined.`);
       console.log(message.data.mnemonic, message.data.address);
+      appendToFile(`mnemonic: ${message.data.mnemonic}\naddrs: ${message.data.address}`);
+
       workers.forEach((worker) => worker.terminate());
     }
   });
